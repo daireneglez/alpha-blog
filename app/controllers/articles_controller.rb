25b20cccb8dc185eb4489dfe88c069f96226513d
dairenemiereles <dmeireles@norwex.com>
2023-03-27 12:11:57 -0500

    Add edit action and fix turbo issue

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   # POST /articles
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -23,6 +27,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] ="Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 end
