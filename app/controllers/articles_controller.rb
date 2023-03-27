76e948770a7d52c0f0709213dd2208cb92972745
dairenemiereles <dmeireles@norwex.com>
2023-03-27 11:40:36 -0500

    Redirect

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,28 @@
 class ArticlesController < ApplicationController
+  #GET /articles/1
   def show
     @article = Article.find(params[:id])
   end
 
+  # GET /articles
   def index
     @articles = Article.all
   end
 
+  # GET /articles/new
   def new
-
+    @article = Article.new
   end
 
+  # POST /articles
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+
+    if @article.save
+      flash[:notice] ="Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 end
