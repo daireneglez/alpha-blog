77c290117b49acd1bf0da3be6ce71fdfc8a9daf0
dairenemiereles <dmeireles@norwex.com>
2023-04-10 16:32:07 -0500

    Add has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_184734) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_212346) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_184734) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
