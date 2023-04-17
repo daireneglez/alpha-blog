5cf5d9ba2c99a2c8a32cd1c7f798a9dfacc2c555
dairenemiereles <dmeireles@norwex.com>
2023-04-17 14:53:45 -0500

    Add destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -27,6 +27,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+
   def edit
 
   end
@@ -44,13 +51,14 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 
   def require_same_user
     if current_user != @user
-      flash[:alert] = "You can only edit your own account"
+      flash[:alert] = "You can only edit or delete your own account"
       redirect_to @user
     end
   end
